8fcec31f4acdc47b149c0347ad829aca760dc8f0
seamong <honghaitzz11@gmail.com>
2018-05-30 16:17:36 +0800

    重新创建刚好-pages分支

--- book.js
+++ book.js
@@ -24,7 +24,7 @@ module.exports = {
 
     pluginsConfig: {
         sitemap: {
-            hostname: "https://toolchain.gitbook.com",
+            hostname: "https://blog.honghaitao.net/",
         },
         "search-pro": {
             cutWordLib: "nodejieba",
